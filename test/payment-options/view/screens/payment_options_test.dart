59e8623f89e1ad2cef21710c612e355fb6969699
rafael.eiki <rafael.eiki@quintoandar.com.br>
2021-09-24 10:04:16 -0300

    Fixed tests for payment screen mocking provider and API calls

--- test/payment-options/view/screens/payment_options_test.dart
+++ test/payment-options/view/screens/payment_options_test.dart
@@ -1,13 +1,46 @@
+import 'package:faturas/payment-options/model/payment_option.dart';
+import 'package:faturas/payment-options/model/payment_options_model.dart';
+import 'package:faturas/payment-options/repository/rest/payment_options_rest_service.dart';
 import 'package:faturas/payment-options/view/screens/payment_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
 
+import 'payment_options_test.mocks.dart';
+
+final getIt = GetIt.instance;
+
+@GenerateMocks([PaymentOptionsRestService])
 void main() {
   group('PaymentOptionsScreen', () {
-    testWidgets('Renders', (tester) async {
-      await tester.pumpWidget(const MaterialApp(home: PaymentOptionsScreen()));
+    final mockService = MockPaymentOptionsRestService();
+    final mockedList = [
+      PaymentOption(1, 3180.0, 3180.0),
+      PaymentOption(2, 1630.0, 3260.0),
+      PaymentOption(3, 1086.67, 3260.0),
+      PaymentOption(4, 815.0, 3260.0),
+      PaymentOption(5, 662.0, 3310.0),
+      PaymentOption(6, 551.67, 3310.0),
+    ];
 
+    when(mockService.fetchPaymentOptions())
+        .thenAnswer((_) => Future.value(mockedList));
+
+    getIt.registerSingleton<PaymentOptionsRestService>(mockService);
+
+    Future render(WidgetTester tester) async {
+      await tester.pumpWidget(ChangeNotifierProvider(
+        create: (_) => PaymentOptionsModel(3180.0),
+        child: const MaterialApp(home: PaymentOptionsScreen()),
+      ));
       await tester.pumpAndSettle();
+    }
+
+    testWidgets('Renders', (tester) async {
+      await render(tester);
 
       expect(
           find.byKey(const Key('payment_options_container')), findsOneWidget);
@@ -24,8 +57,13 @@ void main() {
 
     testWidgets('When a payment option is selected the operation updates',
         (tester) async {
-      await tester.pumpWidget(const MaterialApp(home: PaymentOptionsScreen()));
+      await render(tester);
 
+      await tester.dragUntilVisible(
+        find.byKey(const Key('payment_option_4')),
+        find.byType(ListView),
+        const Offset(0, 50),
+      );
       Finder optionFinder = find.byKey(const Key('payment_option_4'));
       Finder costFinder = find.byKey(const Key('operation_cost'));
 
